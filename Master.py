d2232cbb196472b250999eaa08c2fe901dce37ad
snigsc <snigdhachenjeri00@gmail.com>
2020-11-28 23:41:11 +0530

    listening requests

--- Master.py
+++ Master.py
@@ -1,5 +1,19 @@
 import json 
 import socket
 import sys
-import numpy
 import threading
+
+from socket import *
+serverName = "localhost"
+serverPort = 5000
+masterSocket = socket(AF_INET, SOCK_STREAM)
+
+masterSocket.bind(("localhost",serverPort))
+masterSocket.listen(1)
+print("The Master is ready to receive")
+while 1:
+    connectionSocket, addr = masterSocket.accept()
+    ip = connectionSocket.recv(1024)
+    print(ip)
+ 
+masterSocket.close()
