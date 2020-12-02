afee8b4201298e9a34e8a139698470700509f007
snigsc <snigdhachenjeri00@gmail.com>
2020-12-02 23:08:50 +0530

    with readme

--- Analysis.py
+++ Analysis.py
@@ -3,46 +3,65 @@ import seaborn as sns
 import matplotlib as plt
 from statistics import *
 import sys
+import os.path
 
 mode = sys.argv[1]
-if mode=='ALL':
-    pass
-elif mode=='RANDOM' or mode=='RR' or mode=='LL':
-    jobFile = open(mode+'_joblogs.csv','r')
-    taskFile = open(mode+'_tasklogs.csv','r')
-    next(jobFile)
-    next(taskFile)
-    jobList = []
-    taskList = []
-    jobList.extend([float(jobTime.strip().split(',')[-1]) for jobTime in jobFile])
-    # print (jobList)
-    taskList.extend([float(taskTime.strip().split(',')[-1]) for taskTime in taskFile])
-    # print (taskList)
-    jobFile.close()
-    taskFile.close()
-    meanJobs = mean(jobList)
-    medianJobs = median(jobList)
-    meanTasks = mean(taskList)
-    medianTasks = median(taskList)
-    print('Mean time for jobs: ', meanJobs, '\nMedian time for jobs: ', medianJobs)
-    print('Mean time for tasks: ', meanTasks, '\nMedian time for tasks: ', medianTasks)
-    exit()    
+
+if mode=='1':
+    files = []
+    if os.path.isfile('RANDOM_joblogs.csv') and os.path.isfile('RANDOM_tasklogs.csv'):
+        files.append([open('RANDOM_joblogs.csv','r'),open('RANDOM_tasklogs.csv','r')])
+    if os.path.isfile('RR_joblogs.csv') and os.path.isfile('RR_tasklogs.csv'):
+        files.append([open('RR_joblogs.csv','r'),open('RR_tasklogs.csv','r')])
+    if os.path.isfile('LL_joblogs.csv') and os.path.isfile('LL_tasklogs.csv'):
+        files.append([open('LL_joblogs.csv','r'),open('LL_tasklogs.csv','r')])
+    # print(files)
+    for f in files:
+        next(f[0])
+        next(f[1])
+        jobList = []
+        taskList = []
+        jobList.extend([float(jobTime.strip().split(',')[-1]) for jobTime in f[0]])
+        # print (jobList)
+        taskList.extend([float(taskTime.strip().split(',')[-1]) for taskTime in f[1]])
+        # print (taskList)
+        f[0].close()
+        f[1].close()
+        try:
+            meanJobs = mean(jobList)
+            medianJobs = median(jobList)
+            meanTasks = mean(taskList)
+            medianTasks = median(taskList)
+            i = f[0].name.index('_')
+            print('\nAlgorithm: '+f[0].name[:i])
+            print('Mean time for jobs: ', meanJobs, 's \nMedian time for jobs: ', medianJobs, 's')
+            print('Mean time for tasks: ', meanTasks, 's \nMedian time for tasks: ', medianTasks,'s \n')
+        except:
+            pass
+    exit()   
+elif mode=='2':
+    pass 
 else:
     print('Invalid input')
     exit()
 
 
-fList = [['RANDOM_joblogs.csv','RANDOM_tasklogs.csv'],['RR_joblogs.csv','RR_tasklogs.csv'],['LL_joblogs.csv','LL_tasklogs.csv']]
+files = [['RANDOM_joblogs.csv','RANDOM_tasklogs.csv'],['RR_joblogs.csv','RR_tasklogs.csv'],['LL_joblogs.csv','LL_tasklogs.csv']]
+
+for f in files:
+    if not os.path.isfile(f[0]) or not os.path.isfile(f[1]):
+        print('Some required files are missing for visualization.\nPlease run all three algorithms before entering mode 2 or enter mode 1 for individual analysis.')
+        exit()
 
 jmeanlst = ['Mean_Jobs']
 tmeanlst = ['Mean_Tasks']
 jmedianlst = ['Median_Jobs']
 tmedianlst = ['Median_Tasks']
 
-for f in fList:
+for f in files:
     jobs = pd.read_csv(f[0])
     tasks = pd.read_csv(f[1])
-    jmeanlst.append(mean(list(jobs['JobCompletionTime'])))
+    jmeanlst.append(mean(jobs['JobCompletionTime'].tolist()))
     jmedianlst.append(median(jobs['JobCompletionTime'].tolist()))
     tmeanlst.append(mean(tasks['TaskCompletionTime'].tolist()))
     tmedianlst.append(median(tasks['TaskCompletionTime'].tolist()))
@@ -82,5 +101,5 @@ plt.pyplot.xlim(min(pos)-width, max(pos)+width*4)
 plt.pyplot.ylim([0, max(df['LL'] + df['RR'] + df['RANDOM'])] )
 plt.pyplot.legend(['LL', 'RR', 'RANDOM'], loc='upper left')
 plt.pyplot.savefig('barChart.png')
-print('Done')
+print('Done. Please check current working directory for files:\n barChart.png\n RANDOM_tasklogs.png\n RR_tasklogs.png\n LL_tasklogs.png')
 
---
+++ README.md
@@ -0,0 +1,39 @@
+# YetAnotherCentralizedScheduler
+Big Data Project - December 2020
+
+### Instructions for execution:
+**Files required:**  
+Analysis.py  
+Config.json  
+Master.py  
+Requests.py  
+Workers.py  
+
+1. Five terminals are needed to execute this project. We will refer to them as T1, T2, T3, T4 ad T5.
+
+2. Open 3 terminals, one for each worker node and run the following:  
+T1: `python3 Workers.py 4000 1`  
+T2: `python3 Workers.py 4001 2`  
+T3: `python3 Workers.py 4002 3`  
+
+3. Run the following command on T4 to start the master node:  
+`python3 Master.py Config.json <scheduling_algorithm>`  
+Here, scheduling algorithm can take the following values - LL (least loaded), RANDOM (random) or RR (round robin).
+
+4. Run the following command on T5 to send specified number of job requests to the master:  
+`python3 Requests.py <number_of_requests>`  
+
+5. Repeat steps 2 to 4 for each scheduling algorithm and run the following command on T5 to analyse the results obtained:  
+`python3 Analysis.py <mode>`  
+- Enter mode '1' for individual logs of algorithms - prints mean and median times of already run scheduling algorithms on the terminal.  
+- Enter mode '2' for visual analysis - generates a bar plot comparing the three algorithms and a heat map for each algorithm depicting number of tasks running on each machine at regular time intervals.  
+**Note:** Mode 2 (visual analysis) will only generate plots if all the three algorithms have been tested prior to running analysis.  
+  
+  
+    
+### Collaborators:
+- Ananya V [PES101800204]
+- Sakshi Shetty [PES1201800190]
+- Snigdha S Chenjeri [PES1201800045]
+- Swanuja Maslekar [PES1201800369]
+
